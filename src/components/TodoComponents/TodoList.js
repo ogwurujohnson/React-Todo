02ae633f916f987d7f9bd54077174b62f4763f6f
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-05-30 08:10:49 +0100

    Feat: Build out the todo list component to have both pending and completed todos

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react'
+import Todo from './Todo'
+import CompletedTodo from './completedTodo'
+
+
+export default function TodoList(props) {
+    const { todos, onFinish, completed } = props;
+    return (
+        <div>
+            <h3>Pending Todos</h3>
+            {todos.map(todo => {
+                return <Todo  key={todo.id} todo={todo} onFinish={onFinish} />
+            })}
+            <h3>Completed</h3>
+            {completed.map(todo => {
+                return <CompletedTodo key={completed.key} completed={todo}/>
+            })}
+        </div>
+    )
+}
